f53e05db80e424afe08d18ff69f35057de6b228e
pepemontan7 <jose.a.delgado.lopez@gmail.com>
2016-01-13 20:54:31 -0800

    add consistent color scheme to all activities and remove settings action from detailsactivity

--- app/src/main/java/com/example/jose/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/jose/popularmovies/MainActivity.java
@@ -6,14 +6,17 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Log.v(LOG_TAG, "On create MAIn Act: ");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
